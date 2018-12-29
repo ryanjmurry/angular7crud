6fd49eb26119358029a48b8a42f924b64653b341
Ryan Murry <ryanjmurry@gmail.com>
2018-12-29 15:14:13 -0800

    add business route to server

--- api/server.js
+++ api/server.js
@@ -2,9 +2,10 @@ const express = require('express'),
   path = require('path'),
   bodyParser = require('body-parser'),
   cors = require('cors'),
-  mongoose = require('mongoose');
-config = require('./DB');
+  mongoose = require('mongoose'),
+  config = require('./DB');
 
+const businessRoute = require('./routes/business.route');
 mongoose.Promise = global.Promise;
 mongoose
   .connect(
@@ -16,15 +17,16 @@ mongoose
       console.log('Database is connected');
     },
     err => {
-      console.log('Can not connect to the database ' + err);
+      console.log('Can not connect to the database' + err);
     }
   );
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-let port = process.env.PORT || 4000;
+app.use('/business', businessRoute);
+const port = process.env.PORT || 4000;
 
-const server = app.listen(function() {
+const server = app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
